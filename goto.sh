c98f8a90190fb828e39af7d4fc8dc0812d778272
banement <47301693+BanementI@users.noreply.github.com>
2021-04-15 23:39:30 +0100

    Create goto.sh

---
+++ goto.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Goes to some folders I frequently go to.
+function goto() {
+	#Dummy thingy initially required as empty functions are illegal. Use goto-add.sh to add shortcuts.
+  
+	#Dummy!
+	if [ "$1" = "dummy" ]
+	then
+		cd "/"
+	fi
+}
