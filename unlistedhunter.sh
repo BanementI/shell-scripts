5d8c86e7f874afca44895c947ef7aac92f621180
banement <47301693+BanementI@users.noreply.github.com>
2025-01-28 23:01:43 +0000

    kys

--- unlistedhunter.sh
+++ unlistedhunter.sh
@@ -21,7 +21,7 @@ unlistedhunter() {
 
    if [[ "$1" == "old" ]]; then
         # Extract video IDs from filenames in the old format
-        find . -maxdepth 1 -regex '.*\.\(mkv\|mp4\|webm\)' | sed -E 's|.*/.*-([A-Za-z0-9_-]{11})\.[^.]+$|\1|' | uniq > "$videoList"
+        find . -maxdepth 1 -regex '.*\.\(mkv\|mp4\|webm\)' | sed -En '/.*-[A-Za-z0-9_-]{11}\.[^.]+$/ s/.*-([A-Za-z0-9_-]{11})\.[^.]+$/\1/p' | uniq > "$videoList"
    else
         # Extract video IDs from filenames with square brackets
         find . -maxdepth 1 -regex '.*\.\(mkv\|mp4\|webm\)' | grep '\[[^]]\{11\}\]' | sed -E 's/.*\[([^]]{11})\].*/\1/' | uniq > "$videoList"
